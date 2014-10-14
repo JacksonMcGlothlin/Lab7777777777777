6454b016106f17c77833843dc7653e5fda9f2dd3
JacksonMcGlothlin <jwmcglothlin@me.com>
2014-10-14 14:02:28 -0500

    Lab 7

---
+++ Loops Intro..py
@@ -0,0 +1,27 @@
+mylist = [1,2,3,4,5,6,7,8,9,10]
+for i in mylist:
+   print 'the list is running'
+   print i
+print 'the list is finished'
+
+
+for Q in range(11,21,1):
+    print Q
+print 'the loop is finished'
+
+
+list2 = []
+for W in range(101,0,-2):
+    print W
+print 'the loop is finished'
+
+
+List3 = []
+for S in range (0,5,1):
+    userInput = raw_input ()
+    List3.append(userInput)
+for S in List3:
+    print S
+
+    
+    
